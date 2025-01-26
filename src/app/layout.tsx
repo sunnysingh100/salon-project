fb2317262be4597e3bc2d562d8b9d0685fedc006
Sunny Singh <sunnysingh706050@gmail.com>
2025-01-26 16:26:29 +0530

    add the umami analytics id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import Transition from "@/components/Transition";
 import CursorProvider from "@/components/CursorProvider";
+import Script from "next/script";
 
 const marcellus = Marcellus({
   subsets: ["latin"],
@@ -29,6 +30,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-screen">
+      <Script
+        src="http://localhost:3000/script.js"
+        data-website-id="1809c47f-8628-4d6f-a049-cd17d36d0948"
+        strategy="afterInteractive"
+      />
       <body
         className={`${marcellus.variable} ${montserrat.variable} overflow-x-hidden`}
       >
